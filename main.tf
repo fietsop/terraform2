daddbb5a9876b4333bf030d76817477df1f1180a
alain foryim <alinocooll.af@gmail>
2023-10-11 08:58:18 -0500

    edited

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-    region = "us-east-1a"
+    region = "us-east-1"
   
 }
 resource "aws_vpc" "myvpc" {
-    cidr_block = "10.0.1.0/24"
+    cidr_block = "10.0.1.0/16"
     enable_dns_hostnames = true
     enable_dns_support = true
 
