be68c0fd380be4984ada339c2587e4400419f27c
alain foryim <alinocooll.af@gmail>
2023-10-11 09:00:26 -0500

    fixed

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
   
 }
 resource "aws_vpc" "myvpc" {
-    cidr_block = "10.0.1.0/16"
+    cidr_block = "10.0.0.0/16"
     enable_dns_hostnames = true
     enable_dns_support = true
 
