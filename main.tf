43b13490579c59696203d1e4d336c4a291d767a0
alain foryim <alinocooll.af@gmail>
2023-10-11 08:49:53 -0500

    vpc created

--- main.tf
+++ main.tf
@@ -2,3 +2,10 @@ provider "aws" {
     region = "us-east-1a"
   
 }
+resource "aws_vpc" "myvpc" {
+    cidr_block = "10.0.0.1/24"
+    enable_dns_hostnames = true
+    enable_dns_support = true
+    
+  
+}
